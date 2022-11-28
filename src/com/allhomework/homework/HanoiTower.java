04045fbcf95be78c48937e68df20d5fd4bcdfba2
HuAng <sys_snow@163.com>
2022-11-28 19:23:11 +0800

    JAVA章节作业

--- src/com/allhomework/homework/HanoiTower.java
+++ src/com/allhomework/homework/HanoiTower.java
@@ -1,4 +1,4 @@
-package com.learn.homework;
+package com.allhomework.homework;
 
 /**
  * @author HuAng
---
+++ src/com/allhomework/oophomework/salarywork/Employee.java
@@ -0,0 +1,56 @@
+package com.allhomework.oophomework.salarywork;
+
+/**
+ * @author HuAng
+ */
+public class Employee {//员工类
+    private String name;
+    private double daySalary;
+    private int day;
+    private double grade;
+
+    public Employee(String name, double daySalary, int day, double grade) {
+        this.name = name;
+        this.daySalary = daySalary;
+        this.day = day;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDaySalary() {
+        return daySalary;
+    }
+
+    public void setDaySalary(double daySalary) {
+        this.daySalary = daySalary;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public double inputSalary(){
+        return daySalary;
+    }
+}
+
+   
